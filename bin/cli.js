f4ba80f7ffb9d607763a71901944b65c42cf22f2
Ryan Nguyen <69874455+RyanIII@users.noreply.github.com>
2022-11-30 23:05:35 -0500

    Update cli.js

--- bin/cli.js
+++ bin/cli.js
@@ -1,1 +1,14 @@
+#!/usr/bin/env node
 
+import {roll} from "../lib/roll.js"
+import minimist from "minimist";
+
+const args = minimist(process.argv.slice(2));
+
+const sides = args.sides || 6;
+const dice = args.dice || 2;
+const rolls = args.rolls || 1;
+
+//converts teh output of the roll function to json
+var combine = roll(argv.sides, argv.dice, argv.rolls);
+console.log(JSON.stringfy(combine));
